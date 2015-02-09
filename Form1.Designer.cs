a90d8fc399c19df468f1b3411831ff9c938dc957
anelrz <skobylarz@gmail.com>
2015-02-09 21:13:34 +0100

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyDocuments;
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(506, 273);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.listBox1, 3);
+            this.listBox1.Size = new System.Drawing.Size(419, 267);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(428, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load ISBs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(428, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "RUN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 273);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,18 +3,95 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Excel_to_ISB_preset_files
 {
     public partial class Form1 : Form
     {
+        string[] files;
+        Excel.Application xlApp;
+        Excel.Workbook wb;
+        int number;
+        NumberFormatInfo nfi;
+        StreamWriter sw;
+
         public Form1()
         {
             InitializeComponent();
+            xlApp = new Microsoft.Office.Interop.Excel.Application();
+            nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";
+            sw = new StreamWriter("myxml.xml");
+            sw.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            sw.WriteLine("<presets>");
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                folderBrowserDialog1.ShowDialog();
+                files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.xlsx", SearchOption.AllDirectories);
+                listBox1.Items.AddRange(files);
+            }
+            catch (DirectoryNotFoundException) { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (string file in files)
+            {
+                try
+                {
+                    wb = xlApp.Workbooks.Open(Filename: file, IgnoreReadOnlyRecommended: true);
+                    Excel.Worksheet ws = wb.Worksheets[1];
+                    Console.WriteLine(ws.Name);
+
+                    int lastRow = ws.Cells.Find("*", System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious, false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
+                    
+                    for (int i = 1; i <= lastRow; i++)
+                    {
+                        try
+                        {
+                            string cell = (String) ws.Cells[i, 3].Value2;
+
+                            if (cell[0].CompareTo('M') == 0 && Int32.TryParse(cell[1].ToString(), out number))
+                            {
+                                string value = ws.Cells[i, 4].Value2;
+                                //Console.WriteLine(cell + "  " + value);
+
+                                string[] words = value.Split(' ');
+                                double amp;
+                                Double.TryParse(words[0], NumberStyles.Number, nfi, out amp);
+                                string unit = words[1];
+                                Console.WriteLine(amp.ToString(nfi) + "  " + unit);
+
+                                //sw.Write
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            //Console.WriteLine(exc.Message);
+                        }
+                    }
+
+                    wb.Close();
+                    sw.Close();
+                }
+                catch (System.Runtime.InteropServices.COMException ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                }
+            }
         }
     }
 }
